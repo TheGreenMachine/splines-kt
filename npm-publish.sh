427c912e3e9db50c5e842a31c5a350d9cb418a4e
Richik SC <sc.richik@gmail.com>
2021-06-20 22:36:13 -0500

    Fix npm publish script

--- npm-publish.sh
+++ npm-publish.sh
@@ -1,6 +1,5 @@
-./gradlew compileKotlinJs || exit
+./gradlew build|| exit
 ./gradlew compileProductionExecutableKotlinJs || exit
-./gradlew jsPublicPackageJson || exit
 
 outputDir=build/compileSync/main/productionExecutable/kotlin
 cp build/tmp/jsPublicPackageJson/package.json $outputDir/
